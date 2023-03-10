1866b922ff3e84cb232419ba0387508e3382fe20
xiaoheimaoo <737476225@qq.com>
2023-03-10 16:38:46 +0800

    优化

--- src/main/java/cn/mcfun/utils/OrderExecute.java
+++ src/main/java/cn/mcfun/utils/OrderExecute.java
@@ -49,22 +49,6 @@ public class OrderExecute implements Runnable{
     }
 
     public void login() {
-        String ip = null;
-        try {
-            Connection conn = getConnection();
-            String sql = "select ip from `proxy` order by rand() LIMIT 1";
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                ip = rs.getString("ip");
-            }
-            conn.close();
-            rs.close();
-            ps.close();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        userInfo.setIp(ip);
         Connection conn2 = getConnection();
         String sql2 = "update `order` set status=1 where `order`="+userInfo.getOrder()+" and status!=1";
         PreparedStatement ps2 = null;
@@ -82,63 +66,12 @@ public class OrderExecute implements Runnable{
             }
         }
         UserCreate uc = new UserCreate();
-        uc.usercreate1(userInfo);
-        uc.usercreate2(userInfo);
-        uc.usercreate3(userInfo);
-        uc.usercreate4(userInfo);
-        uc.usercreate5(userInfo);
-        uc.usercreate6(userInfo);
-        uc.usercreate7(userInfo);
-        uc.usercreate8(userInfo);
         //登录
-        uc.usercreate9(userInfo);
-        uc.usercreate10(userInfo);
-        uc.usercreate11(userInfo);
-        uc.usercreate12(userInfo);
-        uc.usercreate14(userInfo);
-
-        uc.usercreate15(userInfo);
-        uc.usercreate16(userInfo);
-        uc.usercreate17(userInfo);
-        uc.usercreate18(userInfo);
-        uc.usercreate19(userInfo);
-        uc.usercreate20(userInfo);
-        uc.usercreate21(userInfo);
-        uc.usercreate22(userInfo);
-        uc.usercreate23(userInfo);
-        uc.usercreate24(userInfo);
-        uc.usercreate25(userInfo);
-        uc.usercreate26(userInfo);
-        uc.usercreate27(userInfo);
-        uc.usercreate28(userInfo);
-        uc.usercreate29(userInfo);
-        uc.usercreate30(userInfo);
-        uc.usercreate31(userInfo);
-        uc.usercreate32(userInfo);
-        uc.usercreate33(userInfo);
-        uc.usercreate34(userInfo);
-        uc.usercreate35(userInfo);
-        uc.usercreate36(userInfo);
-        uc.usercreate37(userInfo);
-        uc.usercreate38(userInfo);
-        uc.usercreate39(userInfo);
-        uc.usercreate40(userInfo);
-        uc.usercreate41(userInfo);
-        uc.usercreate42(userInfo);
-        uc.usercreate43(userInfo);
-        uc.usercreate44(userInfo);
-        uc.usercreate45(userInfo);
-        uc.usercreate46(userInfo);
-        uc.usercreate47(userInfo);
-        uc.usercreate48(userInfo);
-        uc.usercreate49(userInfo);
-        uc.usercreate50(userInfo);
-        uc.usercreate51(userInfo);
-        uc.usercreate52(userInfo);
-        uc.usercreate53(userInfo);
-        uc.usercreate54(userInfo);
-        uc.usercreate55(userInfo);
-        uc.usercreate56(userInfo);
+        uc.accountAuth(userInfo);
+        uc.requestQuestion(userInfo);
+        uc.academyGetinfo(userInfo);
+        uc.accountLoginsync(userInfo);
+        uc.networktimeSync(userInfo);
         if(userInfo.getAttendanceBookRewards().size() >= 1){
             for(int i=0;i<userInfo.getAttendanceBookRewards().size();i++){
                 uc.attendanceReward(Integer.parseInt((String) userInfo.getAttendanceBookRewards().get(i)),1,userInfo);
@@ -146,28 +79,20 @@ public class OrderExecute implements Runnable{
         }
         if(userInfo.getAttendanceHistoryDBs().size() >= 1){
             for(int i=0;i<userInfo.getAttendanceHistoryDBs().size();i++){
-                uc.attendanceReward(Integer.parseInt(userInfo.getAttendanceHistoryDBs().get(i).toString().split("-")[0]),Integer.parseInt(userInfo.getAttendanceHistoryDBs().get(i).toString().split("-")[1]),userInfo);
+                uc.attendanceReward(Integer.parseInt(userInfo.getAttendanceHistoryDBs().get(i).toString().split("-")[0]),Integer.parseInt(userInfo.getAttendanceHistoryDBs().get(i).toString().split("-")[1])+1,userInfo);
             }
         }
-        uc.usercreate59(userInfo);
-        uc.usercreate60(userInfo);
-        uc.usercreate61(userInfo);
-        uc.usercreate62(userInfo);
-        uc.missionMultiplereward(userInfo);
         uc.mailList(userInfo);
         if(!userInfo.getMail().toJSONString().equals("[]")){
             uc.mailReceive(userInfo);
         }
         Connection conn3 = getConnection();
-        String sql3 = "update `order` set uid=?,transcode=?,AccountId=?,`status`=2,`message`=? where `order`=? and status=1";
+        String sql3 = "update `order` set `status`=2,`message`=? where `order`=? and status=1";
         PreparedStatement ps3 = null;
         try {
             ps3 = conn3.prepareStatement(sql3);
-            ps3.setString(1, userInfo.getUid());
-            ps3.setString(2, userInfo.getTranscode());
-            ps3.setString(3, userInfo.getAccountId().toString());
-            ps3.setString(4, "订单已完成");
-            ps3.setString(5, userInfo.getOrder());
+            ps3.setString(1, "订单已完成");
+            ps3.setString(2, userInfo.getOrder());
             ps3.executeUpdate();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
