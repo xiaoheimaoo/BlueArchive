a08c86e4d5f1c842a45b700675d38d55a385c2ab
xiaoheimaoo <737476225@qq.com>
2023-03-17 16:56:24 +0800

    优化

--- blue_archive.sql
+++ blue_archive.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : dsadsa
+ Source Server         : xiaoheimao.mysql.rds.aliyuncs.com_3306
  Source Server Type    : MySQL
- Source Server Version : 80032
- Source Host           : 43.155.94.137:3306
+ Source Server Version : 80018
+ Source Host           : xiaoheimao.mysql.rds.aliyuncs.com:3306
  Source Schema         : blue_archive
 
  Target Server Type    : MySQL
- Target Server Version : 80032
+ Target Server Version : 80018
  File Encoding         : 65001
 
- Date: 12/03/2023 23:29:39
+ Date: 17/03/2023 16:46:56
 */
 
 SET NAMES utf8mb4;
@@ -22,30 +22,31 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `order`;
 CREATE TABLE `order`  (
-  `order` int(0) NOT NULL AUTO_INCREMENT,
-  `uid` int(0) NULL DEFAULT NULL,
-  `transcode` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
-  `AccountId` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
-  `status` int(0) NULL DEFAULT NULL,
-  `content` int(0) NULL DEFAULT NULL,
-  `current` int(0) NULL DEFAULT NULL,
-  `message` varchar(500) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `order` int(20) NOT NULL AUTO_INCREMENT,
+  `uid` int(10) NULL DEFAULT NULL,
+  `transcode` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `AccountId` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `status` int(1) NULL DEFAULT NULL,
+  `content` int(20) NULL DEFAULT NULL,
+  `current` int(20) NULL DEFAULT NULL,
+  `message` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `timerecovery` datetime(0) NULL DEFAULT NULL,
-  `StarNum` int(0) NULL DEFAULT NULL,
-  `ip` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `StarNum` int(5) NULL DEFAULT NULL,
+  `Gem` int(10) NULL DEFAULT NULL,
+  `svts` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `complete` datetime(0) NULL DEFAULT NULL,
-  `SessionKey` varchar(500) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
-  `deviceId` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `SessionKey` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `deviceId` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`order`) USING BTREE,
   UNIQUE INDEX `order`(`order`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1000198507 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 1000098754 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for proxy
 -- ----------------------------
 DROP TABLE IF EXISTS `proxy`;
 CREATE TABLE `proxy`  (
-  `ip` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL
-) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
+  `ip` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 SET FOREIGN_KEY_CHECKS = 1;
