b75830644b4ddbcf93cd66468e45fe784fa24e52
xiaoheimaoo <737476225@qq.com>
2023-03-18 21:18:21 +0800

    优化

--- src/main/java/cn/mcfun/entity/StudentName.java
+++ src/main/java/cn/mcfun/entity/StudentName.java
@@ -4,28 +4,26 @@ import com.alibaba.fastjson.JSONArray;
 import java.io.*;
 
 public class StudentName {
-    static JSONArray a = new JSONArray();
-    public StudentName(){
-        String jsonStr = "";
-        try {
-            File jsonFile = new File("students.json");
-            FileReader fileReader = new FileReader(jsonFile);
-            Reader reader = new InputStreamReader(new FileInputStream(jsonFile),"utf-8");
-            int ch = 0;
-            StringBuffer sb = new StringBuffer();
-            while ((ch = reader.read()) != -1) {
-                sb.append((char) ch);
-            }
-            fileReader.close();
-            reader.close();
-            jsonStr = sb.toString();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        a = JSONArray.parseArray(jsonStr);
-    }
+    static String jsonStr = null;
     public static String getStudentName(int id){
-
+        if(jsonStr == null){
+            try {
+                File jsonFile = new File("students.json");
+                FileReader fileReader = new FileReader(jsonFile);
+                Reader reader = new InputStreamReader(new FileInputStream(jsonFile),"utf-8");
+                int ch = 0;
+                StringBuffer sb = new StringBuffer();
+                while ((ch = reader.read()) != -1) {
+                    sb.append((char) ch);
+                }
+                fileReader.close();
+                reader.close();
+                jsonStr = sb.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        JSONArray a = JSONArray.parseArray(jsonStr);
         String b = null;
         for(int i=0;i<a.size();i++){
             if(a.getJSONObject(i).getInteger("Id") == id){
