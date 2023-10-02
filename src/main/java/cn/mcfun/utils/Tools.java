46032873b92aaba1bab725314b62b0c6eb4a0c67
xiaoheimaoo <737476225@qq.com>
2023-10-02 14:19:30 +0800

    优化

--- src/main/java/cn/mcfun/utils/Tools.java
+++ src/main/java/cn/mcfun/utils/Tools.java
@@ -70,11 +70,13 @@ public class Tools {
      * @return int值
      */
     public static int byteArrayToInt(byte[] bytes) {
-        int value=0;
-        for(int i = 0; i < 4; i++) {
-            int shift= (3-i) * 8;
-            value +=(bytes[i] & 0xFF) << shift;
+        if (bytes == null || bytes.length != 4) {
+            throw new IllegalArgumentException("Invalid byte array");
         }
-        return value;
+
+        return ((bytes[3] & 0xFF) << 24) |
+                ((bytes[2] & 0xFF) << 16) |
+                ((bytes[1] & 0xFF) << 8)  |
+                (bytes[0] & 0xFF);
     }
 }
