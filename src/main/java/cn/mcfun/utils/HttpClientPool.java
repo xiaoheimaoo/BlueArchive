e4b97dcd0c9ed241fd70454c4667cda667c3bc2b
xiaoheimaoo <737476225@qq.com>
2023-11-18 14:31:53 +0800

    优化

--- src/main/java/cn/mcfun/utils/HttpClientPool.java
+++ src/main/java/cn/mcfun/utils/HttpClientPool.java
@@ -3,36 +3,21 @@ package cn.mcfun.utils;
 import cn.mcfun.Main;
 import cn.mcfun.entity.UserInfo;
 import org.apache.http.*;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.config.CookieSpecs;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.config.Registry;
-import org.apache.http.config.RegistryBuilder;
-import org.apache.http.conn.socket.ConnectionSocketFactory;
-import org.apache.http.conn.socket.PlainConnectionSocketFactory;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.*;
 import org.apache.http.impl.conn.DefaultProxyRoutePlanner;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.ssl.SSLContexts;
 import org.apache.http.util.EntityUtils;
-
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -47,23 +32,16 @@ public class HttpClientPool {
         List<NameValuePair> params = new ArrayList<>();
         params.add(new BasicNameValuePair("captcha_id", "00b06e0a4ed58bd1c2ad59f1b054ade0"));
         String result = null;
-        try {
-            httpPost.setEntity(new UrlEncodedFormEntity(params));
-            response = httpClient.execute(httpPost);
-            result = EntityUtils.toString(response.getEntity(), Charset.forName("utf-8"));
-        } catch (IOException e) {
+        int num = 0;
+        while(num == 0){
             try {
-                Connection conn2 = getConnection();
-                String sql2 = "update `order` set message='网络异常，正在重试!',status=0 where `order`=? and status=1";
-                PreparedStatement ps2 = conn2.prepareStatement(sql2);
-                ps2.setString(1,userInfo.getOrder());
-                ps2.executeUpdate();
-                conn2.close();
-                ps2.close();
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
+                httpPost.setEntity(new UrlEncodedFormEntity(params));
+                response = httpClient.execute(httpPost);
+                result = EntityUtils.toString(response.getEntity(), Charset.forName("utf-8"));
+                num = 1;
+            } catch (IOException e) {
+                num = 0;
             }
-            Thread.currentThread().stop();
         }
         return result;
     }
